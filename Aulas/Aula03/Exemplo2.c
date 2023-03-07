e3c09bdbd4d969c7df2688fdc7f674d480b099c3
lucasrodri <lucasrc.rodri@gmail.com>
2023-03-07 20:42:36 -0300

    ajuste no algoritmo da Aula 3

--- Aulas/Aula03/Exemplo2.c
+++ Aulas/Aula03/Exemplo2.c
@@ -5,11 +5,15 @@
 int main (void) {
     char str[101], str1[51], str2[51];
     printf("Entre com uma seqüência de caracteres: ");
-    scanf(" %50s[^\n]", str1);
+    scanf("%50[^\n]", str1);
+    printf("Primeira string lida: %s\n", str1);
+    fflush(stdin); // limpa o buffer do teclado
     printf("Entre com outra seqüência de caracteres: ");
-    scanf(" %50s[^\n]", str2);
+    scanf("%50[^\n]", str2);
+    printf("Segunda string lida: %s\n", str2);
     copia(str, str1);
     concatena(str, str2);
+    printf("Strings concatenadas: %s\n", str);
     printf("Comprimento total: %d\n",comprimento(str));
     return 0;
 }
