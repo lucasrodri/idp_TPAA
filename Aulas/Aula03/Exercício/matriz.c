282d39c23f378c4a1eb7d738e20623ddf7216d93
lucasrodri <lucasrc.rodri@gmail.com>
2023-03-09 00:36:29 -0300

    Resolução da Tarefa da Aula 3 TADs

--- Aulas/Aula03/Exercício/matriz.c
+++ Aulas/Aula03/Exercício/matriz.c
@@ -3,26 +3,47 @@
 #include "matriz.h"
 
 void libera (Matriz* mat) {
-    //TODO: Criar a função!
+    free(mat->v);  
+    free(mat);
 }
 
 Matriz* cria (int m, int n) {
-    //TODO: Criar a função!
+    Matriz* mat = (Matriz*) malloc(sizeof(Matriz));
+    if (mat == NULL) {
+        printf("Memória insuficiente!\n");  
+        exit(1);
+    }
+    mat->lin = m;
+    mat->col = n;
+    mat->v = (float*) malloc(m*n*sizeof(float));
+    return mat;
 }
 
 float acessa (Matriz* mat, int i, int j) {
-    //TODO: Criar a função!
+    int k; /* índice do elemento no vetor – armazenamento por linha*/
+    if (i<0 || i>=mat->lin || j<0 || j>=mat->col) {
+        printf("Acesso inválido!\n");
+        exit(1);
+    }
+    k = (i)*mat->col + (j);
+    return mat->v[k];
 }
 
 int linhas (Matriz* mat) {
-    //TODO: Criar a função!
+    return mat->lin;
 }
 
 void atribui (Matriz* mat, int i, int j, float v) {
-    //TODO: Criar a função!
+    int k; /* índice do elemento no vetor */
+    if (i<0 || i>=mat->lin || j<0 || j>=mat->col) {
+        printf("Atribuição inválida!\n");  
+        exit(1);
+    }
+    k = (i)*mat->col + (j);
+    mat->v[k] = v;
 }
 
 int colunas (Matriz* mat) {
-    //TODO: Criar a função!
+    return mat->col;
 }
 
--- Aulas/Aula03/Exercício/programa.c
+++ Aulas/Aula03/Exercício/programa.c
@@ -7,16 +7,30 @@ int main(int argc, char *argv[]) {
     Matriz *M;
 
     // criação de uma matriz  
-    //TODO: Faça o código!
+    M = cria(5,5);
 
     // inserção de valores na matriz
-    //TODO: Faça o código!
+    atribui(M,0,0,100);
+    atribui(M,0,1,200);
+    atribui(M,0,2,300);
+    atribui(M,0,3,400);
+    atribui(M,0,4,500);
+    atribui(M,1,2,40);
+    atribui(M,2,3,3);
+    atribui(M,3,4,15);
+    atribui(M,4,1,22);
 
     // verificando se a inserção foi feita corretamente  
-    //TODO: Faça o código!
+    a = acessa(M,1,2);
+    b = acessa(M,2,3);
+    c = acessa(M,3,4);
+    d = acessa(M,4,1);
 
-    //Mostre valores da Matriz criada
-    //TODO: Faça o código!
+    printf ("M[0][0]: %4.2f \n", acessa(M,0,0));
+    printf ("M[1][2]: %4.2f \n", a);
+    printf ("M[2][3]: %4.2f \n", b);
+    printf ("M[3][4]: %4.2f \n", c);
+    printf ("M[4][1]: %4.2f \n", d);
     
     return 0;
 }
